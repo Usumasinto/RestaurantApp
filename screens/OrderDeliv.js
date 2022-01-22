ff5911a646acaa77213fffd0370200f6307a9b48
Daniel <ing.danielrmz@hotmail.com>
2022-01-22 02:17:43 -0500

    commit message

---
+++ screens/OrderDeliv.js
@@ -0,0 +1,15 @@
+import React from "react"
+import {
+    View,
+    Text
+} from "react-native"
+
+const OrderDeliv = () => {
+    return (
+        <View>
+            <Text>Home</Text>
+        </View>
+    )
+}
+
+export default OrderDeliv;
---
+++ screens/index.js
@@ -0,0 +1,10 @@
+
+import Home from './home'
+import Restaurant from './Restaurant'
+import OrderDeliv from './OrderDeliv'
+
+export {
+    Home,
+    Restaurant,
+    OrderDeliv
+}
